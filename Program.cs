505a5f4cb9e67b8ac002e75e55eb62d6007940f5
J. Gabriel Maia <jonagabriel@gmail.com>
2020-08-01 17:38:05 -0300

    Brincadeira com a Letícia! Chamamos o PC de robô e criamos a função escreva e ensinamos ele a idade da Letícia, do Gabriel, quantos cubos tinha na cama e o trabalho do Gabriel.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace leticia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> escreva = Console.WriteLine;
+
+            var paragrafo = "\n";
+            
+            var agora = DateTime.Now;
+
+            Func<int,int,int,DateTime> nascimento = (dia, mes, ano) => new DateTime(ano,mes,dia);
+
+            var idadeDaLeticia = agora - nascimento(10,06,2014);
+            var idadeDoGabriel = agora - nascimento(20,10,1993);
+
+            var quantosDiasTemUmAno = 365;
+            var quantosDiasEuVivi = idadeDaLeticia.Days;
+
+            var quantosDiasGabrielViveu = idadeDoGabriel.Days;
+
+
+            var quantosAnosTenho = quantosDiasEuVivi / quantosDiasTemUmAno;
+            
+            var quantosAnosGabrielTem = quantosDiasGabrielViveu / quantosDiasTemUmAno;
+
+            var quantoCubosTem = 5 + 14;
+            var trabalhoDoGabriel = 2020 - 2018;
+
+
+            escreva(paragrafo);
+            escreva("Oi, meu nome é Letícia!");
+            escreva("Minha idade é " + quantosAnosTenho + " anos");
+            escreva("O Gabriel é velho e tem "+ quantosAnosGabrielTem + " anos");
+            escreva("Eu tenho " + quantoCubosTem + " cubos!");
+            escreva("O trabalho do Gabriel tem " + trabalhoDoGabriel + " anos já!");
+            escreva(paragrafo);
+        }
+    }
+}
